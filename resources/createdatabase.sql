7f833866e6a34a82ebea27f89aaef7cd37e19332
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2020-12-12 22:56:34 +0100

    Delete createdatabase.sql

--- resources/createdatabase.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE wpdb;
-
-CREATE USER 'wpuser'@'localhost' IDENTIFIED BY 'wppwd';
-GRANT ALL ON wpdb.* TO 'wpuser'@'localhost' IDENTIFIED BY 'wppwd' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
