4ee0db7946736f1dacbe938ad3a171cf4422f70d
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2020-12-12 22:55:22 +0100

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# USER=filezilla
-# USERID=1000
-# echo "creating new $USER with UID $USERID"
-# useradd -m $USER -u $USERID 
-# chown -R $USER /home/$USER
-# cd /home/$USER 
-# su $USER -c filezilla
